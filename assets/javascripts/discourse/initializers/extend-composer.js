c352222ddc7251d00edafc66ad1b6abe7148105e
Jake Goldsborough <jake.goldsborough@pm.me>
2026-07-25 12:18:05 -0700

    fix: serialize timezone and cost fields

--- assets/javascripts/discourse/initializers/extend-composer.js
+++ assets/javascripts/discourse/initializers/extend-composer.js
@@ -3,17 +3,21 @@ import { withPluginApi } from "discourse/lib/plugin-api";
 // Fields the composer needs to send to the server when creating or
 // editing an itinerary topic. Mirrors DiscourseItinerary::CUSTOM_FIELDS
 // on the Ruby side.
-const ITINERARY_FIELDS = [
+export const ITINERARY_FIELDS = [
   "itinerary_item_type",
   "itinerary_parent_trip_id",
   "itinerary_starts_at",
   "itinerary_ends_at",
+  "itinerary_start_timezone",
+  "itinerary_end_timezone",
   "itinerary_origin",
   "itinerary_destination",
   "itinerary_name",
   "itinerary_location",
   "itinerary_confirmation_code",
   "itinerary_status",
+  "itinerary_cost_amount",
+  "itinerary_cost_currency",
 ];
 
 export default {
